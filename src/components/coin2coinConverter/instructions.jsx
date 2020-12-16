94f37468ea8a4f4017b197834820372413ee918d
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2020-12-16 09:49:22 -0800

    updated spelling error

--- src/components/coin2coinConverter/instructions.jsx
+++ src/components/coin2coinConverter/instructions.jsx
@@ -4,7 +4,7 @@ function Instructions() {
 
     return (
         <div className='instructions'>
-            <h3>The rules are simple. Choose two differnt types of cryptocurrency from the current top 10, enter the amount of units you wish to convert, and see that sweet sweet conversion rate</h3>
+            <h3>The rules are simple. Choose two different types of cryptocurrency from the current top 10, enter the amount of units you wish to convert, and see that sweet sweet conversion rate</h3>
         </div>
     )
 }
